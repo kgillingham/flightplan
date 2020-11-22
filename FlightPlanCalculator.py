4cf78729ed115535eb7c5a584a6d96aea8e0aeae
kgillingham <kazuto.gillingham@flemingcollege.ca>
2020-11-22 11:25:07 -0500

    Skeleton outline of the program

--- FlightPlanCalculator.py
+++ FlightPlanCalculator.py
@@ -1,1 +1,18 @@
+# This program will aid the designing process of an aerial photography remote sensing mission. 
+# Specifically, this program will calculate the location, direction, and number of flight lines
+# necessary to adequately photograph a given area, as well as the elevation the aircraft should fly at. 
 
+def main():
+    print('test')
+
+# TO DO
+def haversine():
+    print('test')
+
+# TO DO
+def latLongDistance():
+    print('test')
+
+
+if __name__ == "__main__":
+    main()
